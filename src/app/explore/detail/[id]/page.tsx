b8b7cbce5e5e341ab5b9399a984591af9f1e4a14
jw-r <evencoding@gmail.com>
2025-06-30 11:50:51 +0900

    feat: og

--- src/app/explore/detail/[id]/page.tsx
+++ src/app/explore/detail/[id]/page.tsx
@@ -1,3 +1,61 @@
-export default function ExploreDetailPage() {
-  return <div>Explore Detail</div>;
+export interface GetPublicSingleDocumentQuizDto {
+  id: number;
+  answer: string;
+  question: string;
+  explanation: string;
+  options: string[];
+  quizType: "MIX_UP" | "MULTIPLE_CHOICE";
+}
+
+export interface GetPublicSingleDocumentResponse {
+  id: number;
+  creator: string;
+  name: string;
+  emoji: string;
+  category: string;
+  tryCount: number;
+  bookmarkCount: number;
+  totalQuizCount: number;
+  isBookmarked: boolean;
+  createdAt: string;
+  quizzes: GetPublicSingleDocumentQuizDto[];
+  isOwner: boolean;
+}
+
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const { id } = params;
+  const response = await fetch(
+    `${process.env.API_URL!}/documents/${id}/public`
+  );
+  const data: GetPublicSingleDocumentResponse = await response.json();
+
+  return {
+    title: "픽토스: 나를 성장시키는 AI 퀴즈",
+    description: "나를 성장시키는 똑똑한 퀴즈",
+    openGraph: {
+      title: data.name,
+      type: "article",
+      url: `https://picktoss.com/explore/detail/${id}`,
+      siteName: "PickToss",
+      locale: "ko_KR",
+      images: [
+        {
+          url: "/opengraph-quiz.png",
+          width: 1200,
+          height: 630,
+          alt: data.name,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: data.name,
+      description: "나를 성장시키는 똑똑한 퀴즈",
+      images: ["/opengraph-quiz.png"],
+    },
+  };
+}
+
+export default async function ExploreDetailPage() {
+  return null;
 }
